5721763f38fe75dd4e4eafac9fcfdf96727aeb13
Sang2306 <sangvnorg@gmail.com>
2019-04-15 17:44:47 +0700

    Added 2d, 3d coor system \n Convert real coordinates to machine one

---
+++ main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <cmath>
+#include <graphics.h>
+struct Point{
+    int x;
+    int y;
+    Point();
+    Point(int x, int y);
+};
+Point::Point()
+{
+    x = y = -1;
+}
+Point::Point(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+void bresenham(Point p1, Point p2, bool solid = true);
+void draw2DCoor();
+void draw3DCoor();
+void realToMachine(int& x, int& y);
+/////////////////////////////////
+int width = 1200, height = 600;
+int midX = (int)width/2;
+int midY = (int)height/2;
+int main(int argc, char* argv[]){
+    initwindow(width, height, "KYTHUATDOHOA");
+    draw2DCoor();
+    int x1 = -2, y1 = 2, x2 = 12, y2 = 12;
+    realToMachine(x1, y1);
+    realToMachine(x2, y2);
+    bresenham(Point(x1, y1), Point(x2, y2));
+    //draw3DCoor();
+    getch();
+    closegraph();
+    return EXIT_SUCCESS;
+}
+void realToMachine(int& x, int& y)
+{
+    /*
+        Chuyen diem x,y thanh toa do may tinh
+        5 : don vi
+    */
+    x = x*5 + midX;
+    if(y < 0)
+        y = y*5*-1 + midY;
+    else
+        y = midY - y*5;
+}
+
+void bresenham(Point p1, Point p2, bool solid)
+{
+    /*
+        solid: neu dung thi ve lien net, nguoc lai ve ----
+        thuat toan tren wikipedia
+    */
+   int x0 = p1.x, y0 = p1.y, x1 = p2.x, y1 = p2.y;
+   int dx = abs(x1-x0);
+   int dy = abs(y1-y0);
+   int sx, sy;
+   if(x0 < x1) sx = 1; else sx = -1;
+   if(y0 < y1) sy = 1; else sy = -1;
+   int err = dx - dy;
+   int e2, count = 4;
+   while(true)
+   {
+       if(!solid)
+       {
+
+           if(count == 4)
+           {
+               putpixel(x0, y0, WHITE);
+           }
+           count--;
+           if (count == 0){
+                count = 4;
+           }
+       }else
+            putpixel(x0, y0, WHITE);
+
+       if(x0 == x1 && y0 == y1) break;
+       e2 = 2*err;
+       if(e2 > -dy)
+       {
+           err = err - dy;
+           x0 = x0 + sx;
+       }
+       if(e2 < dx)
+       {
+           err = err + dx;
+           y0 = y0 + sy;
+       }
+   }
+}
+
+void draw2DCoor()
+{
+    bresenham(Point(0, midY), Point(width, midY), false);
+    bresenham(Point(midX, 0), Point(midX, height), false);
+}
+
+void draw3DCoor()
+{
+    bresenham(Point(midX, 0), Point(midX, midY));
+    //////////////////////////////////////////////////
+    bresenham(Point(0, height), Point(midX, midY), false);
+    bresenham(Point(width, height), Point(midX, midY), false);
+}
