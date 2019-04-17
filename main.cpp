ce7bd39f6dd0814f634a0112e1a3ab4855c97866
Sang2306 <sangvnorg@gmail.com>
2019-04-17 10:19:45 +0700

    them ham translateCompute va add label cho truc toa do

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <cstdio>
 #include <graphics.h>
 #include "consolelib.h"
 struct Point{
@@ -8,10 +9,12 @@ struct Point{
     Point();
     Point(int x, int y);
 };
+
 Point::Point()
 {
     x = y = -1;
 }
+
 Point::Point(int x, int y)
 {
     this->x = x;
@@ -21,17 +24,26 @@ void bresenham(Point p1, Point p2, int color, bool solid=true);
 void draw2DCoor();
 void draw3DCoor();
 void realToMachine(int& x, int& y);
+void translateCompute(int& x, int& y, int& h, int tr_x, int tr_y);
 void chooseCoorSystem();
 void chooseObject2Draw();
-/////////////////////////////////
-int width = 1200, height = 600;
+//////////GLOBAL VAR///////////
+int width = 1300, height = 680;
 int midX = (int)width/2;
 int midY = (int)height/2;
 //////////////////////////////////////MAIN FUNC////////////////////////////////////////////////////
 int main(int argc, char* argv[]){
     initwindow(width, height, "KYTHUATDOHOA");
     chooseCoorSystem();
-    chooseObject2Draw();
+    //chay thu thuat toan tinh tien
+    int x = 1, y = 1, h = 1, tr_x = -16, tr_y = -16;
+    int z = x, c = y;
+    realToMachine(z, c);
+    putpixel(z,c,YELLOW);
+    translateCompute(x,y,h,tr_x, tr_y);
+    realToMachine(x, y);
+    putpixel(x,y,LIGHTCYAN);
+    //chooseObject2Draw();
     getch();
     closegraph();
     return EXIT_SUCCESS;
@@ -77,6 +89,19 @@ void realToMachine(int& x, int& y)
         y = y*5*-1 + midY;
     else
         y = midY - y*5;
+}
+
+void translateCompute(int& x, int& y, int& h, int tr_x, int tr_y)
+{
+    int vector[3] = {x, y, h};
+    int translate[3][3] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {tr_x, tr_y, 1},
+    };
+    x = vector[0]*translate[0][0] + vector[1]*translate[1][0] + vector[2]*translate[2][0];
+    y = vector[0]*translate[0][1] + vector[1]*translate[1][1] + vector[2]*translate[2][1];
+    h = vector[0]*translate[0][2] + vector[1]*translate[1][2] + vector[2]*translate[2][2];
 }
 
 void bresenham(Point p1, Point p2, int color, bool solid)
@@ -125,7 +150,7 @@ void bresenham(Point p1, Point p2, int color, bool solid)
                         y = y - 1;
                     px = px + 2*(dy1 - dx1);
                }
-               count = count==0 ? 5 : count-1;
+               count = count==1 ? 5 : count-1;
           }
      }
      else //tim x theo y vi chieu ngang nho hon (m >= 1)
@@ -163,20 +188,20 @@ void bresenham(Point p1, Point p2, int color, bool solid)
                         x = x - 1;
                     py = py + 2*(dx1 - dy1);
                }
-               count = count==0 ? 5 : count-1;
+               count = count==1 ? 5 : count-1;
           }
      }
 }
 
 void draw2DCoor()
 {
-    bresenham(Point(0, midY), Point(width, midY), WHITE, false);//X
-    bresenham(Point(midX, 0), Point(midX, height), WHITE, false);//Y
+    bresenham(Point(0, midY), Point(width, midY), WHITE, false); outtextxy(width-12, midY, "X");//X
+    bresenham(Point(midX, 0), Point(midX, height), WHITE, false); outtextxy(midX, 0, "Y");      //Y
 }
 
 void draw3DCoor()
 {
-    bresenham(Point(midX, 0), Point(midX, midY), WHITE, false);     //Y
-    bresenham(Point(0, height), Point(midX, midY), WHITE, false);   //Z
-    bresenham(Point(width, height), Point(midX, midY), WHITE, false);//X
+    bresenham(Point(midX, 0), Point(midX, midY), WHITE, false); outtextxy(midX, 0, "Y");                   //Y
+    bresenham(Point(0, height), Point(midX, midY), WHITE, false); outtextxy(8, height-24, "Z");            //Z
+    bresenham(Point(width, height), Point(midX, midY), WHITE, false); outtextxy(width-12, height-24, "X"); //X
 }
